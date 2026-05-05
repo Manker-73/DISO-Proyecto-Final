f08b58e1732af3884fb220182dfa5f1f841c890b
SaraLimaC <ayelen6666@gmail.com>
2026-05-06 01:11:55 +0200

    QuickAttackDecorator

--- src/main/java/main/decorator/QuickAttackDecorator.java
+++ src/main/java/main/decorator/QuickAttackDecorator.java
@@ -8,10 +8,15 @@ public class QuickAttackDecorator extends ActionDecorator{
     public QuickAttackDecorator(Action accionEnvuelta){
         super(accionEnvuelta);
     }
+    public CombatResult executeAction(Character origen, Character destino) {
+        CombatResult resultadoAtaqueAgil1 = accionEnvuelta.executeAction(origen, destino);
+        if (origen.getAgilidad() >= 15) {
+            CombatResult resultadoAtaqueAgil2 = accionEnvuelta.executeAction(origen, destino);
 
-    if( origen.getAgilidad()>15){
-
+            double danoTotal = resultadoAtaqueAgil1.getDamage() + resultadoAtaqueAgil2.getDamage();
+            CombatResult resultadoAtaquesAgiles = new CombatResult(danoTotal, resultadoAtaqueAgil1.getNewState(),resultadoAtaqueAgil1.isBlocking());
+            return resultadoAtaquesAgiles;
+        }
+        return resultadoAtaqueAgil1;
     }
-    //@Override
-
 }
