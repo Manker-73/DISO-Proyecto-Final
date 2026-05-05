17f1b72b33153c305f250cbb776ddc89a11e1ca6
SaraLimaC <ayelen6666@gmail.com>
2026-05-05 02:14:24 +0200

    Heal

---
+++ src/main/java/main/actions/Heal.java
@@ -0,0 +1,11 @@
+package main.actions;
+import main.abstracta.Character;
+import main.combat.CombatResult;
+
+public class Heal implements Action{
+    @Override
+    public CombatResult executeAction(Character origen, Character destino) {
+        origen.setVida(origen.getVida() + 15);
+        return new CombatResult(0, null, false);
+    }
+}
